8600b0ae51865a7a845da654126726dc669d9850
Jayred Degoma <jayreddegoma27@gmail.com>
2025-11-16 23:27:20 +0800

    new code

---
+++ DeclaringVar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    float height = 5.0;
+    double weight = 55.55;
+    char name[] = "Jayred";
+    
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Height: %f\n", height);
+    printf("Weight: %lf\n", weight);
+    
+    return 0;
+}
